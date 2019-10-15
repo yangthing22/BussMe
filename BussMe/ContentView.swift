e1f96cc50e8e19ce0a8f3b40b2fe8b2bb244cc55
Mulyanti Law <yangthing22@gmail.com>
2019-10-15 15:25:35 +0700

    Initial Commit

---
+++ BussMe/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BussMe
+//
+//  Created by Mulyanti Law on 15/10/19.
+//  Copyright © 2019 Mulyanti Law. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ BussMeTests/BussMeTests.swift
@@ -0,0 +1,34 @@
+//
+//  BussMeTests.swift
+//  BussMeTests
+//
+//  Created by Mulyanti Law on 15/10/19.
+//  Copyright © 2019 Mulyanti Law. All rights reserved.
+//
+
+import XCTest
+@testable import BussMe
+
+class BussMeTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
